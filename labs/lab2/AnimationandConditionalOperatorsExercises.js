122f01b091ea780e254f428e4bbf0dcb76e05bd5
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2020-09-13 18:31:33 -0400

    lab 2 update

--- labs/lab2/AnimationandConditionalOperatorsExercises.js
+++ labs/lab2/AnimationandConditionalOperatorsExercises.js
@@ -1,3 +1,5 @@
+//PUCK SIDE****
+
 // settting the size of the circle:
 var radius = 20;
 
@@ -9,4 +11,29 @@ function setup() {
 
 function draw() {
   ellipse(mouseX, mouseY, radius, radius);
+}
+
+//WORLD WRAP****
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(200);
+
+  // Draw a circle
+  stroke(50);
+  fill(100);
+  ellipse(x, y, 24, 24);
+
+  // Jiggling randomly on the horizontal axis
+  x = x + random(-1, 1);
+  // Moving up at a constant speed
+  x = x - 1;
+
+  // Reset to the bottom
+  if (x < 0) {
+    x = width;
+  }
 }
