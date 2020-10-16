6e2540ccf76f3d6bd9f72b0a61595d0f90160cfb
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2020-10-16 18:40:46 -0400

    arrays js 2 done

--- labs/lab4/arrays.js
+++ labs/lab4/arrays.js
@@ -1,4 +1,4 @@
-//neapol**********************************************************
+//Neapolitan**********************************************************
 
 let arrayColors = ["#0099ff", "#e600ff", "#00ff51"];
 
@@ -10,5 +10,22 @@ function draw() {
   for (var count = 0; count < arrayColors.length; count++) {
     fill(arrayColors[count]);
     rect(80 + 100 * count, 80, 80);
+  }
+}
+
+//snake**********************************************************
+let LastPos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function setup() {
+  creatCanvas(500, 400);
+}
+
+function draw() {
+  background();
+  LastPos.push(mouseX);
+  LastPos.shift();
+
+  for (var count = 0; count < LastPos.length; count++) {
+    circle(LastPos[count], 150, 20);
   }
 }
