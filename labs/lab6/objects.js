68fee45ad32d404983379f093638ab3d494dd7a8
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2020-10-29 16:09:53 -0400

    lab6 update

--- labs/lab6/objects.js
+++ labs/lab6/objects.js
@@ -1,20 +1,20 @@
 let mySquare = [
 
-{x: 100, y: 30, w: 50, h: 50},
-{x: 100, y: 30, w: 50, h: 50},
-{x: 100, y: 30, w: 50, h: 50}
-];
-
-function setup(){
-
-    createCanvas(400,300);
-
-}
-
-function draw(){
-
-    for(let count =0; count > mySquare.length; i++){
-    let nSquare = mySquare[count];
-    rect(mySquare[i].x,mySquare[i].y,mySquare[i].r);
+    {x: 80, y: 30, color:"#EED038"},
+    {x: 50, y: 30, color:"#A338EE"},
+    {x: 60, y: 30, color:"#38C1EE"}
+    ];
+    
+    function setup(){
+    
+        createCanvas(400,300);
+    
     }
-}
+    
+    function draw(){
+    background(200)
+        for(let count =     0; count > mySquare.length; count++){
+        let nSquare = mySquare[count];
+        rect(mySquare[count].x,mySquare[count].y,mySquare[count].color);
+        }
+    }
