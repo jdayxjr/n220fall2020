68e4e119986de80ddfb42a9bfa1614544e7a88cf
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2020-10-29 14:29:55 -0400

    lab5 update

--- labs/lab5/functions.js
+++ labs/lab5/functions.js
@@ -18,15 +18,47 @@ function draw() {
 //bounce off**********************************************************
 
 
+let xPos = 0;
+let yPos = 0;
 
-function setup(){
-createCanvas(350,150);
-background();
+let xSpeed =2;
+let ySpeed= 2;
 
+
+
+
+// sets the speed of the ball x and y
+
+function setup() {
+  createCanvas(300, 200);
 }
 
-function draw () {
+function draw() {
+  background(220);
+  
+  xPos = xPos + xSpeed;
+  yPos = yPos + ySpeed;
+  
+  // makes the ball move ^^^ 
+  
+  
+  if (xPos > width || xPos < 0) {
+    xSpeed = xSpeed * -1;
+  //multiply by negative one to alternate bt +/- 
+  //this makes the ball bounce to each side
+}
+  
+  if (yPos > height || yPos < 0){
+    ySpeed = ySpeed *-1;
 
-
-
+    //multiply by negative one to alternate bt +/- 
+  //this makes the ball bounce to each side
+  }
+  
+  fill(27,86,237);
+  circle(xPos,yPos,30);
+  // draws the circle and the position of the ball where its supposed to be.
+  
+  
+  
 }
