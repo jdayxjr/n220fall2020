36b5054f26f023ffd3a3af6c2c1f5b790642de1a
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2020-12-12 20:45:41 -0500

    final update 2

--- labs/FinalProject/app.js
+++ labs/FinalProject/app.js
@@ -1,110 +1,108 @@
 //Declared the variables to hold the card array data
-var memoryArr = ['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8'];
-var memoryValues = [];
-var memoryCardIds = [];
-var tiles_flipped = 0;
+var ArrayData = ['12', '12', '2', '2', '33', '33', '45', '45', '53', '53', '61', '61', '71', '71', '89', '89'];
+var dataValue = [];
+var DataCard_ids = [];
+var CardsFlipped = 0;
+
+
 //prototype to shuffle the array values
-Array.prototype.memory_shuffle = function(){
-var i = this.length, j, temp;
-while(--i > 0){
-j = Math.floor(Math.random() * (i+1));
-temp = this[j];
-this[j] = this[i];
-this[i] = temp;
-}
+Array.prototype.memory_shuffle = function () {
+    var i = this.length, j, temp;
+    while (--i > 0) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
 
 }
-//Flip card and find match
-function flipCard(cardTile, val){
-debugger;
-if(cardTile.innerHTML == "" && memoryValues.length < 2){
-cardTile.innerHTML = val;
+//finding the each cards matching card
+function Card_Flip(cardSlot, value) {
+    debugger;
+    if (cardSlot.innerHTML == "" && dataValue.length < 2) {
+        cardSlot.innerHTML = value;
 
-if(memoryValues.length == 0){
-memoryValues.push(val);
-memoryCardIds.push(cardTile.id);
+        if (dataValue.length == 0) {
+            dataValue.push(value);
+            DataCard_ids.push(cardSlot.id);
 
-} 
+        }
 
-else if(memoryValues.length == 1){
-memoryValues.push(val);
-memoryCardIds.push(cardTile.id);
+        else if (dataValue.length == 1) {
+            dataValue.push(value);
+            DataCard_ids.push(cardSlot.id);
 
-if(memoryValues[0] == memoryValues[1]){
-tiles_flipped += 2;
+            if (dataValue[0] == dataValue[1]) {
+                CardsFlipped += 2;
 
-// Clear both arrays
+                // Clearing the arrays
 
-memoryValues = [];
-memoryCardIds = [];
+                dataValue = [];
+                DataCard_ids = [];
 
-// Check to see if the whole board is cleared
-
-if(tiles_flipped == memoryArr.length){
-
-
-}
-
-} 
+                // Checking to see if the board has been cleared
+                if (CardsFlipped == ArrayData.length) {
 
 
-else {
+                }
 
-function flip2Back(){
+            }
 
-// Flip the 2 tiles back over
 
-var tile_1 = document.getElementById(memoryCardIds[0]);
-var tile_2 = document.getElementById(memoryCardIds[1]);
+            else {
 
-tile_1.innerHTML = "";
-tile_2.innerHTML = "";
+                function CardsFlipBack() {
 
-// Clear both arrays
+                    // cards flip back over when wrong
+                    var tile_1 = document.getElementById(DataCard_ids[0]);
+                    var tile_2 = document.getElementById(DataCard_ids[1]);
 
-memoryValues = [];
-memoryCardIds = [];
+                    tile_1.innerHTML = "";
+                    tile_2.innerHTML = "";
 
-}
+                    // Clear both arrays
 
-setTimeout(flip2Back, 700);
+                    dataValue = [];
+                    DataCard_ids = [];
 
-}
+                }
 
-}
+                setTimeout(CardsFlipBack, 700);
 
-}
+            }
+
+        }
+
+    }
 
 }
 
 //This method triggers when all elements inside the html loaded
 
-function LoadElements()
+function LoadElements() {
 
-{
+    ArrayData.memory_shuffle();
+    var card = document.querySelectorAll(".card");
 
-memoryArr.memory_shuffle();
-var card = document.querySelectorAll(".card");
+    //loop through cards put same listener on e
 
-//loop through cards put same listener on e
+    for (var i = 0; i < card.length; i++) {
+        card[i].setAttribute("id", "card_" + i + "_" + ArrayData[i]);
 
-for(var i = 0; i < card.length; i++) {
-card[i].setAttribute("id", "card_" + i + "_" + memoryArr[i]);
-
-}
+    }
 
 }
 
 //Dom listener to set the click event of card
 
-document.addEventListener('DOMContentLoaded', function() {
-document.body.addEventListener('click', function ( event ) {
-if(event.srcElement.className == 'card' ) {
-var idElement = event.srcElement.id.split("_");
-flipCard(event.srcElement, idElement[2]);
+document.addEventListener('DOMContentLoaded', function () {
+    document.body.addEventListener('click', function (event) {
+        if (event.srcElement.className == 'card') {
+            var idElement = event.srcElement.id.split("_");
+            Card_Flip(event.srcElement, idElement[2]);
 
-};
+        };
 
-});
+    });
 
 });
